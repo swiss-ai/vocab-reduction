ccfa578f6d603ee0e77f34673dc71d78aee8d32a
Faruk Zahiragic <faruk.zahiragic@epfl.ch>
2026-04-24 19:38:45 +0200

    Enable HF repo creation when pushing to remote.

--- model_checkpoint_gen.py
+++ model_checkpoint_gen.py
@@ -5,6 +5,7 @@ token = os.environ.get("HF_TOKEN")
 repo_id = os.environ.get("REPO_ID")
 
 login(token=token)
+create_repo(repo_id=repo_id, repo_type="model", exist_ok=True, private=True)
 
 upload_folder(
     folder_path=os.environ.get("FOLDER_PATH"),
